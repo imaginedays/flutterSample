68aa6d727f1727407399958a3817ed58798de3df
黄可 <huangke@jd.com>
2018-10-30 19:31:59 +0800

    加入flutter与native app 交互功能
    Signed-off-by: 黄可 <huangke@jd.com>

--- lib/View/PageTwo.dart
+++ lib/View/PageTwo.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
 
 var globalPicUrl;
 
@@ -29,12 +31,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static const platform = const MethodChannel('samples.flutter.io/battery');
+  String _batteryLevel = 'Unknown battery level.';
+
+  Future<Null> _getBatteryLevel() async {
+    print('start');
+    String batteryLevel;
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    return setUpBodyContainer();
+  }
+
+  //设置body
+  Widget setUpBodyContainer() {
     return new Scaffold(
       body: new Center(
         child: Column(
           children: <Widget>[
+            Text('手机当前电量${_batteryLevel}'),
+            RichText(
+              text: TextSpan(
+                  text: '这是一段富文本',
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: '粗',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    TextSpan(text: '富'),
+                  ]),
+            ),
             Image.network(
               globalPicUrl,
               width: 100.0,
@@ -50,6 +86,12 @@ class _MyHomePageState extends State<MyHomePage> {
                 '回去',
                 style: TextStyle(color: Colors.white),
               ),
+            ),
+            new RaisedButton(
+              onPressed: () {
+                _getBatteryLevel();
+              },
+              child: Text('获取手机电量', style: TextStyle(color: Colors.white)),
             ),
           ],
         ),
