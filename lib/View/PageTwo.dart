23ae326731512e03ee15d49f0c175a50088588e9
黄可 <huangke@jd.com>
2018-10-30 16:18:01 +0800

    全局参数使用
    Signed-off-by: 黄可 <huangke@jd.com>

--- lib/View/PageTwo.dart
+++ lib/View/PageTwo.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
+var globalPicUrl;
+
 class PageTwo extends StatelessWidget {
   final Color color;
   final String movieName;
-  PageTwo(this.color, this.movieName);
+  final String picUrl;
+  PageTwo(this.color, this.movieName, this.picUrl) {
+    globalPicUrl = picUrl;
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -28,12 +33,26 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Center(
-        child: new RaisedButton(
-            onPressed: () {
-              Navigator.pop(context, 'Movie');
-            },
-            color: Colors.lightBlueAccent,
-            child: new Text('回去', style: TextStyle(color: Colors.white))),
+        child: Column(
+          children: <Widget>[
+            Image.network(
+              globalPicUrl,
+              width: 100.0,
+              height: 150.0,
+              fit: BoxFit.fill,
+            ),
+            new RaisedButton(
+              onPressed: () {
+                Navigator.pop(context, 'Movie');
+              },
+              color: Colors.lightBlueAccent,
+              child: Text(
+                '回去',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
